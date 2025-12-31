ae90a6e5dc41ccf46008003ca3e50f0425416644
Abhishek Kale <kaleabhishek2104@gmail.com>
2025-12-31 07:14:01 +0530

    Update component imports and structure in App.jsx
    Reorganize component imports and rendering order in App component.

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,13 @@ import "./styles/global.css";
 
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
-import Experience from "./components/Experience";
 import About from "./components/About";
-import Education from "./components/Education";
 import Skills from "./components/Skills";
-import ExtraCurricular from "./components/ExtraCurricular";
-import TestScores from "./components/TestScores";
 import Projects from "./components/Projects";
+import Experience from "./components/Experience";
+import Education from "./components/Education";
+import TestScores from "./components/TestScores";
+import ExtraCurricular from "./components/ExtraCurricular";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
 
@@ -16,15 +16,17 @@ export default function App() {
   return (
     <>
       <Navbar />
+
       <Hero />
       <About />
+      <Skills />
+      <Projects />
       <Experience />
       <Education />
-      <Skills />
-      <ExtraCurricular />
       <TestScores />
-      <Projects />
+      <ExtraCurricular />
       <Contact />
+
       <Footer />
     </>
   );
